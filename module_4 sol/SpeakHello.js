43d04ea9619b7d217644a5aef134635dd1b3d48d
Helmiamore <80383127+Helmiamore@users.noreply.github.com>
2022-10-03 14:52:54 +0800

    Update SpeakHello.js

--- module_4 sol/SpeakHello.js
+++ module_4 sol/SpeakHello.js
@@ -1,10 +1,10 @@
-(function (window) {
-  var speakWord = "Hello";
-  var helloSpeaker = {
-    speak: function (name) {
-      console.log(speakWord + " " + name);
-    }
-  };
-  window.helloSpeaker = helloSpeaker;
-})(window);
+(function(window){
+	var helloSpeaker = {};
+	var speakWord = "Hello";
+	helloSpeaker.speak = function speak(name) {
+  		console.log(speakWord + " " + name);
+	}
 
+window.helloSpeaker = helloSpeaker;
+
+})(window);
